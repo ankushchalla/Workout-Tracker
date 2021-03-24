63941bd4cf3d60acaf1675661555b13c014d0bf5
Ankush <ankushchalla@gmail.com>
2021-03-24 14:27:27 -0700

    deploying

--- Develop/server.js
+++ Develop/server.js
@@ -13,11 +13,19 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout_tracker", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout_tracker",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+
+);
 
 require('./routes/api_routes')(app);
 require('./routes/html_routes')(app);
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-  });
+  console.log(`App running on port ${PORT}!`);
+});
