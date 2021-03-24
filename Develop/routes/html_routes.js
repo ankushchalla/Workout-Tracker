c022d237a67c104c61a4053e84a6d9c85f4ba5c3
Ankush <ankushchalla@gmail.com>
2021-03-24 14:49:29 -0700

    Fixed html routes.

--- Develop/routes/html_routes.js
+++ Develop/routes/html_routes.js
@@ -2,12 +2,12 @@ const path = require('path');
 
 module.exports = (app) => {
     app.get('/', (req, res) => {
-        res.sendFile('index.html', { root: '\public' });
+        res.sendFile('index.html', { root: path.join('Develop', 'public')});
     })
     app.get('/exercise', (req, res) => {
-        res.sendFile('exercise.html', { root: '\public' });
+        res.sendFile('exercise.html', { root: path.join('Develop', 'public')});
     })
     app.get('/stats', (req, res) => {
-        res.sendFile('stats.html', { root: '\public' });
+        res.sendFile('stats.html', { root: path.join('Develop', 'public')});
     })
 }
