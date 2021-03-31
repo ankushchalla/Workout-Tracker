edd89a8e70a30bbfb70d6990002a9275594c000a
Ankush <ankushchalla@gmail.com>
2021-03-31 15:05:44 -0700

    Fixing route issues.

--- Develop/routes/html_routes.js
+++ Develop/routes/html_routes.js
@@ -2,12 +2,12 @@ const path = require('path');
 
 module.exports = (app) => {
     app.get('/', (req, res) => {
-        res.sendFile('index.html', { root: path.join('Develop', 'public')});
+        res.sendFile(path.join(__dirname, "../public/stats.html"));
     })
     app.get('/exercise', (req, res) => {
-        res.sendFile('exercise.html', { root: path.join('Develop', 'public')});
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
     })
     app.get('/stats', (req, res) => {
-        res.sendFile('stats.html', { root: path.join('Develop', 'public')});
+        res.sendFile(path.join(__dirname, "../public/index.html"));
     })
 }
